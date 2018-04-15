c01b7f94769185a7847fefc5ce70e168cbfcf39c
m31271n <m31271n@users.noreply.github.com>
2018-04-15 22:36:28 +0800

    feat: usable

--- test/index.js
+++ test/index.js
@@ -1,14 +1,34 @@
 import test from 'ava';
 import React from 'react';
-import { shallow, configure } from 'enzyme';
+import { mount, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-
-import Stage from '../src/index.jsx';
+import browserEnv from 'browser-env';
+import Stage from '../src';
 
 configure({ adapter: new Adapter() });
+browserEnv();
 
-test('general usage', t => {
-  const msg = 'testing message';
-  const wrapper = shallow(<Stage content={msg} />);
-  t.is(wrapper.contains(<div>{msg}</div>), true);
+test('type="render" && show=true : stage is rendered', t => {
+  const wrapper = mount(<Stage type="render" show={true} />);
+  const stage = wrapper.find('.stage');
+  t.is(stage.is('.stage'), true);
+});
+
+test('type="render" && show=true : stage is not rendered', t => {
+  const wrapper = mount(<Stage type="render" show={false} />);
+  const stage = wrapper.find('.stage');
+  t.is(stage.length, 0);
+});
+
+test('type="display" && show=true : stage is rendered', t => {
+  const wrapper = mount(<Stage type="display" show={true} />);
+  const stage = wrapper.find('.stage');
+  t.is(stage.is('.stage'), true);
+});
+
+test('type="display" && show=false : stage is rendered but hidden', t => {
+  const wrapper = mount(<Stage type="display" show={false} />);
+  const stage = wrapper.find('.stage');
+  t.is(stage.is('.stage'), true);
+  t.is(stage.prop('style').opacity, 0);
 });
