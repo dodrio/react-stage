9af183953fa2af4e706c290101f18ae43477a649
m31271n <m31271n@users.noreply.github.com>
2018-04-18 23:21:16 +0800

    feat: add StageToggle

--- src/index.jsx
+++ src/index.jsx
@@ -1,73 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const DISPLAY = 'display';
-const RENDER = 'render';
-
-const generateStyle = (type, show) => {
-  const defaultStyle = {
-    position: 'absolute',
-    width: '100%',
-    height: '100%',
-  };
-
-  const displayShowStyle = {
-    transform: 'translateX(0)',
-    opacity: 1,
-  };
-
-  const displayHideStyle = {
-    transform: 'translateX(-1000%)',
-    opacity: 0,
-  };
-
-  let additionalStyle = {};
-  if (type === DISPLAY && show) {
-    additionalStyle = displayShowStyle;
-  } else if (type === DISPLAY && !show) {
-    additionalStyle = displayHideStyle;
-  }
-
-  const style = Object.assign(defaultStyle, additionalStyle);
-  return style;
-};
-
-class Stage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      style: {},
-    };
-  }
-
-  componentWillMount() {
-    const { type, show } = this.props;
-    this.setState({ style: generateStyle(type, show) });
-  }
-
-  componentWillReceiveProps(nextProps) {
-    const { type, show } = nextProps;
-    this.setState({ style: generateStyle(type, show) });
-  }
-
+export class StageToggle extends React.Component {
   render() {
-    const { type, show, children, className = '' } = this.props;
+    const { condition, children } = this.props;
 
-    if (type === RENDER && !show) {
-      return null;
+    if (condition) {
+      return children;
     } else {
-      return (
-        <section className={`stage ${className}`} style={this.state.style}>
-          {children}
-        </section>
-      );
+      return null;
     }
   }
 }
 
-Stage.propTypes = {
-  type: PropTypes.oneOf([RENDER, DISPLAY]),
-  show: PropTypes.bool.isRequired,
+StageToggle.propTypes = {
+  condition: PropTypes.bool.isRequired,
+  children: PropTypes.node,
 };
 
-export default Stage;
+const defaultStyle = {
+  position: 'absolute',
+  width: '100%',
+  height: '100%',
+};
+
+export const Stage = props => {
+  let { children, className } = props;
+  className = className ? `stage ${className}` : 'stage';
+
+  return (
+    <section className={className} style={defaultStyle}>
+      {children}
+    </section>
+  );
+};
+
+Stage.propTypes = {
+  children: PropTypes.node,
+};
--- test/index.js
+++ test/index.js
@@ -3,32 +3,27 @@ import React from 'react';
 import { mount, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import browserEnv from 'browser-env';
-import Stage from '../src';
+import { StageToggle, Stage } from '../src';
 
 configure({ adapter: new Adapter() });
 browserEnv();
 
-test('type="render" && show=true : stage is rendered', t => {
-  const wrapper = mount(<Stage type="render" show={true} />);
+test('condition = true', t => {
+  const wrapper = mount(
+    <StageToggle condition={true}>
+      <Stage />
+    </StageToggle>
+  );
   const stage = wrapper.find('.stage');
   t.is(stage.is('.stage'), true);
 });
 
-test('type="render" && show=true : stage is not rendered', t => {
-  const wrapper = mount(<Stage type="render" show={false} />);
+test('condition = false', t => {
+  const wrapper = mount(
+    <StageToggle condition={false}>
+      <Stage />
+    </StageToggle>
+  );
   const stage = wrapper.find('.stage');
   t.is(stage.length, 0);
-});
-
-test('type="display" && show=true : stage is rendered', t => {
-  const wrapper = mount(<Stage type="display" show={true} />);
-  const stage = wrapper.find('.stage');
-  t.is(stage.is('.stage'), true);
-});
-
-test('type="display" && show=false : stage is rendered but hidden', t => {
-  const wrapper = mount(<Stage type="display" show={false} />);
-  const stage = wrapper.find('.stage');
-  t.is(stage.is('.stage'), true);
-  t.is(stage.prop('style').opacity, 0);
 });
